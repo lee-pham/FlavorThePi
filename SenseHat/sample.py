3f72e621e0004fdd09dbd0d43fc92f00649e21db
Lee <lee-pham@users.noreply.github.com>
2016-06-13 14:02:09 -0500

    Update sample.py

--- SenseHat/sample.py
+++ SenseHat/sample.py
@@ -1,10 +1,13 @@
 from sense_hat import SenseHat
-
+import time
 sense = SenseHat()
 sense.clear()
 
 while True:
     x, y, z = sense.get_accelerometer_raw().values()
+    time.sleep(1)
+    x2, y2, z2 = sense.get_accelerometer_raw().values()
+    x, y, z = x2 - x, y2 - y, z2 - z
     mag = (x ** 2 + y ** 2 + z ** 2) ** 0.5
     print(mag),
 
